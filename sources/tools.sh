dd6ec627d1636dda5bbc3d4282f5f5b1e7025210
gibbyb <gib@gibbyb.com>
2024-12-31 08:39:05 -0600

    Add packages for hyprland

--- sources/tools.sh
+++ sources/tools.sh
@@ -1,2 +1,6 @@
 source ~/.local/share/sunhat/sources/env.sh
+sudo dnf copr enable solopasha/hyprland -y
+sudo dnf copr enable matte-schwartz/sunshine -y
+sudo dnf copr enable pgdev/ghostty -y
+dnf copr enable tomaszgasior/mushrooms -y
 sudo dnf install -y $TOOLS
