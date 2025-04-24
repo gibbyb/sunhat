cb4e8dec441bb928f14aa14e97b9b98cb790e2a7
Gib <gib@gibbyb.com>
2025-04-24 12:30:12 -0500

    Fix open in terminal to go to kitty

--- sources/remove_packages.sh
+++ sources/remove_packages.sh
@@ -1,3 +1,6 @@
 sudo dnf remove -y gnome-contacts gnome-tour \
     gnome-maps libreoffice-calc libreoffice-writer \
     libreoffice-impress totem
+
+sudo mv /usr/bin/gnome-terminal /usr/bin/gnome-terminal.NOPE
+sudo cp /usr/bin/kitty /usr/bin/gnome-terminal
